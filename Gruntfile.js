678bf16ae5ad831afe46d7856dd8553d91bfbfb6
Átila Camurça <camurca.home@gmail.com>
2015-10-29 21:03:31 -0300

    Replace de `@@sigedir` para o diretório correspondente.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,11 @@
 'use strict';
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
     require('time-grunt')(grunt);
 
     // configurable paths
     grunt.initConfig({
-
         clean: {
             dist: {
                 files: [{
@@ -18,6 +17,22 @@ module.exports = function (grunt) {
             }
         },
 
+        replace: {
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'sigedir',
+                            replacement: '/var/www/sige'
+                        }
+                    ]
+                },
+                files: [
+                    {expand: true, flatten: false, src: ['dist/**/*.html']}
+                ]
+            }
+        },
+
         shell: {
             dist: {
                 command: 'gitbook build ./repository dist'
@@ -25,16 +40,14 @@ module.exports = function (grunt) {
         },
 
         'gh-pages': {
-          options: {
-            base: 'dist'
-          },
-          src: ['**']
+            options: {
+                base: 'dist'
+            },
+            src: ['**']
         }
-
-
 
     });
 
-    grunt.registerTask('deploy', ['clean', 'shell', 'gh-pages']);
-    grunt.registerTask('build', ['clean', 'shell']);
+    grunt.registerTask('deploy', ['clean', 'shell', 'replace', 'gh-pages']);
+    grunt.registerTask('build', ['clean', 'shell', 'replace']);
 };
